7a1f5ace3ece5aef4ff8e240b4b05476a82cc356
Atul Lonkar <114733142+atullonkar@users.noreply.github.com>
2023-03-16 21:26:52 +0530

    Update data.js

--- backend/data.js
+++ backend/data.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcryptjs';
 const data = {
     users: [
         {
-          name: 'Owais',
+          name: 'Atul',
           email: 'admin@example.com',
           password: bcrypt.hashSync('123456'),
           isAdmin: true,
